fc8008188569f5652d2c5f244600695f53623115
Austin <53409880+TheRealGoodLivin@users.noreply.github.com>
2021-08-18 18:57:12 +0200

    Update spHelper.js

--- spHelper.js
+++ spHelper.js
@@ -1,7 +1,28 @@
 /*
-    AUTHOR: Austin Livengood
     DATE: 22 Jul 2021
     UPDATED: 5 Aug 2021
+    
+    MIT License
+
+    Copyright (c) 2021 Austin Livengood
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
     NOTE: if the script you are using is on the same site as the list, remove siteURL variable from function. It will grab the same site URL.
 */
